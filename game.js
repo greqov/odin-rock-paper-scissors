8714c6d237c2dd8bb6c6192579ee136fea9d8c91
Ivan Greqov <idgreq@bk.ru>
2022-01-13 14:12:06 +0300

    Get user selection

--- game.js
+++ game.js
@@ -2,15 +2,31 @@ console.log('Horray!')
 
 /*
 TODO:
-    [] get user input (R, P, S) via prompt
-    [] *handle wrong user input
+    [x] get user input (R, P, S) via prompt
+    [x] *handle wrong user input
+    [x] add button to ask a prompt
     [] get computer selection
     [] define round winner
     [] play N rounds until somebody wins 5 times
 */
 
+document.addEventListener('click', (el) => {
+    if (el.target.id === "playButton") {
+        let playerSelection = playerPlay()
+    }
+})
+
 function playerPlay() {
-    // get player selection
+    const availableChoices = ['R', 'P', 'S']
+    let input = prompt('Make your selection: R - rock, P - paper, S - scissors').toUpperCase()
+    if (!availableChoices.includes(input)) {
+        console.log('Wrong input, please try again')
+        console.log('Available options: R, r, P, p, S, s');
+        return
+    }
+
+    console.log(`Player selection: ${input}`);
+    return input
 }
 
 function computerPlay() {
