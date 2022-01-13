0176bef9039254acb062125ec7854f65a925fe17
Ivan Greqov <idgreq@bk.ru>
2022-01-13 13:46:57 +0300

    Add todos

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
 console.log('Horray!')
+
+/*
+TODO:
+    [] get user input (R, P, S) via prompt
+    [] *handle wrong user input
+    [] get computer selection
+    [] define round winner
+    [] play N rounds until somebody wins 5 times
+*/
+
+function playerPlay() {
+    // get player selection
+}
+
+function computerPlay() {
+    // return random value
+}
+
+function playRound(playerSelection, computerSelection) {
+    // magic here
+    // return win/lose message
+}
+
+function playGame() {
+    // play rounds until 5 wins
+}
